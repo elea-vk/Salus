4ac4f6bda478b71959d83d7781457091861e0a58
OmarNsouli <omarnsouli6@gmail.com>
2026-02-25 14:46:20 -0500

    Create musculation.tsx

---
+++ Projet_Salus_officiel_code/Salus/app/musculation.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function MusculationScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 22, fontWeight: "700" }}>Musculation 💪</Text>
+    </View>
+  );
+}
