4d4e4df47208e710aee8435d310f4b82a3a96f9e
elea-vk <elea.verdonk@gmail.com>
2026-04-22 14:05:47 -0400

    update ouverture

--- Projet_Salus_officiel_code/Salus/app/ouverture.tsx
+++ Projet_Salus_officiel_code/Salus/app/ouverture.tsx
@@ -19,7 +19,7 @@ export default function Ouverture() {
   }, []);
 
 
-  const skipInscription = false
+  const skipInscription = true
   
 
 
---
+++ Projet_Salus_officiel_code/Salus/app/pageInscription.tsx
@@ -0,0 +1,9 @@
+import { View,Text } from "react-native";
+
+export default function Inscription () {
+    return(
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <Text>Page Inscription</Text>
+        </View>
+    )
+}
